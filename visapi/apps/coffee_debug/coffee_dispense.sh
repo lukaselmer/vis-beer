1ce7a2e89bb56ed4bc87de0947c3923ee0891f16
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-24 18:10:23 +0100

    delete old setup...

--- visapi/apps/coffee_debug/coffee_dispense.sh
+++
@@ -1,4 +0,0 @@
-
-# Use .netrc file for auth
-curl https://api.vis.ethz.ch/coffee/status/096603@rfid.ethz.ch/dispensed --insecure -n -X POST
-
--- visapi/apps/coffee_debug/status.sh
+++
@@ -1,6 +0,0 @@
-
-# Use .netrc file for auth
-curl https://api.vis.ethz.ch/beer/status/096603@rfid.ethz.ch --insecure -n
-curl https://api.vis.ethz.ch/coffee/status/096603@rfid.ethz.ch --insecure -n
-
-
