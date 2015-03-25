41e5d02accdefe9060c727596c354ae559388e45
Lukas Elmer <lukas.elmer@gmail.com>
2015-03-25 01:32:01 +0100

    data service scaffold

--- visbeer/services/data_service.py
+++ visbeer/services/data_service.py
@@ -3,7 +3,17 @@ class DataService:
         pass
 
     def find_person(self, rfid):
+        # TODO: implement this, use vislib
         return None
 
-    def flag_values(self, person):
+    def get_flag_value(self, person, flag_name):
+        # TODO: implement this, use flags
         return None
+
+    def set_flag_value(self, person, flag_name, new_value):
+        # TODO: implement this, use flags
+        return None
+
+    def set_default_flag_value(self, person, flag_name, new_default_value):
+        if not self.get_flag_value(person, flag_name):
+            self.set_flag_value(person, flag_name, new_default_value)
